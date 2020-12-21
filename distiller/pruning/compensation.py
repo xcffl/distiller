1db5ddc1d5413842fc40f2e4d25339a7938f32b8
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 16:48:19 +0800

    1

---
+++ distiller/pruning/compensation.py
@@ -0,0 +1,25 @@
+import distiller
+
+class CompensatePrunner(object):
+    def __init__(self, name, sensitivities, criteria, threshold, bn_lambda, **kwargs):
+        self.name = name
+        self.sensitivities = sensitivities
+        self.criteria = criteria # choose which criterion to use at first masking step
+        self.threshold = threshold
+        self.bn_lambda = bn_lambda # parameter used after batch normalization layer
+
+    def set_param_mask(self, param, param_name, zeros_mask_dict, meta):
+        if param_name not in self.sensitivities:
+            if '*' not in self.sensitivities:
+                return
+            else:
+                sensitivity = self.sensitivities['*']
+        else:
+            sensitivity = self.sensitivities[param_name]
+
+        # first mask the tensor based on sensitivity criterion.
+        zeros_mask_dict[param_name].mask = distiller.create_mask_similarity_criterion(param, sensitivity)
+
+        threshold = self.threshold
+        # compensate masked elements with elements kept.
+        zeros_mask_dict[param_name].mask = distiller.create_mask_similarity_criterion(param, threshold)
